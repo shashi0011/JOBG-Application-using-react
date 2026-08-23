c7d91aac42043d775842bafbcff231af76cddbd5
shashi0011 <kantshashi3898@gmail.com>
2026-08-23 18:35:51 +0530

    Fix cookie settings on logout response

--- backend/users/views.py
+++ backend/users/views.py
@@ -90,7 +90,7 @@ class LogoutView(APIView):
             
         )
         response.set_cookie(
-            key="token", value="", max_age=0, httponly=True
+            key="token", value="", max_age=0, httponly=True,
             samesite=settings.COOKIE_SAMESITE, secure=settings.COOKIE_SECURE
         )
         return response
