231bcd47161a6bab9148a79d0ee27716e094fc3c
shashi0011 <kantshashi3898@gmail.com>
2026-08-23 18:51:38 +0530

    Update views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,7 +4,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.exceptions import ValidationError
-from django.conf import settings
 
 
 from .serializers import UserSerializer
